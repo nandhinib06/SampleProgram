82a40ca5ca1a8a5a1075f50435b2ff14b931c7b0
nandhinib06 <naganandhini@fourbends.com>
2023-09-15 15:39:33 +0530

    add

---
+++ src/Sample/Main.java
@@ -0,0 +1,17 @@
+package Sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			    int first = 10;
+			    int second = 20;
+
+			    // add two numbers
+			    int sum = first + second;
+			    System.out.println(first + " + " + second + " = "  + sum);
+			  }
+			}
+
+	
